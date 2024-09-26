bdf0fef3c7cd68a16f33c062f58fbc74a121a0c8
biillskiii <muhamad.nabil20052003@gmail.com>
2024-09-26 13:57:20 +0700

    add : Browse Route React

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/Login";
-import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
+import LandingPage from "./pages/LandingPage";
+import SignUp from "./pages/SignUp";
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </Router>
   );
