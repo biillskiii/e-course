358267392d2e841fb1386ca311ff866d177a4cff
biillskiii <muhamad.nabil20052003@gmail.com>
2024-09-27 16:09:39 +0700

    add : label payment comp

---
+++ src/components/Label.jsx
@@ -0,0 +1,43 @@
+import clsx from "clsx";
+
+export default function Label({
+  type = "text",
+  label,
+  size = "full",
+  variant,
+}) {
+  const baseStyle = {
+    baseLabel: [
+      "px-6",
+      "py-3",
+      "rounded-full",
+      "flex",
+      "justify-center",
+      "items-center",
+      "font-bold",
+    ],
+  };
+
+  return (
+    <div
+      className={clsx(baseStyle.baseLabel, {
+        // Size
+        "w-full": size === "full",
+        "w-[208px]": size === "very-big",
+        "w-[194px]": size === "big",
+        "w-[136px]": size === "small",
+        "w-[55px]": size === "very-small",
+
+        // Variants
+        "text-alert-warning bg-alert-warningLight": variant === "pending",
+        "text-alert-success bg-alert-successLight": variant === "successful",
+        "text-alert-danger bg-alert-dangerLight": variant === "failed",
+
+        // Custom logic for type prop
+        "custom-class-based-on-type": type === "Text", // Example usage
+      })}
+    >
+      {label} {/* Render the label */}
+    </div>
+  );
+}
