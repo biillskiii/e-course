f906ddaa387f13503b89441f68e49ef6f18e9c8e
biillskiii <muhamad.nabil20052003@gmail.com>
2024-09-27 16:10:14 +0700

    add : Checkbox comp

--- src/pages/TestComponents.jsx
+++ src/pages/TestComponents.jsx
@@ -1,20 +1,22 @@
 import { Home } from "iconsax-react";
 import Button from "../components/Button";
-
+import Label from "../components/Label";
+import InputBase from "../components/InputForm";
 function App() {
   return (
-    <div className="space-y-5">
+    <div className="space-y-5 px-10">
+      {/* Button */}
       <p>Regular button</p>
       <Button label="Button" variant="primary" />
       <Button label={"Button"} variant="secondary" />
       <Button label={"Button"} variant="tertiary" />
       <Button label={"Button"} variant="disable" />
-
+      {/* Submenu button */}
       <p>Submenu Button</p>
       <Button label={"Button"} variant="submenu" />
       <Button label={"Button"} variant="submenu-active" />
       <Button label={"Button"} variant="submenu-disable" />
-
+      {/* Icon Button */}
       <p>Regular button with Icon</p>
       <Button
         label="Button"
@@ -40,7 +42,7 @@ function App() {
         leftIcon={<Home />}
         rightIcon={<Home />}
       />
-
+      {/* Submenu */}
       <p>Submenu Button with Icon</p>
       <Button
         label={"Button"}
@@ -60,6 +62,13 @@ function App() {
         leftIcon={<Home />}
         rightIcon={<Home />}
       />
+      {/* Label */}
+      <p>Label</p>
+      <Label label="Label" variant={"failed"} size="very-big" />
+      <p>Checkbox</p>
+      <InputBase type={"checkbox"} />
+      <InputBase type={"checkbox"} disabled={true} />
+      {/* Sidebar */}
       <div className="w-60 min-h-screen bg-white shadow-lg flex flex-col items-center">
         <h1 className="mango text-center text-secondary-500 text-[40px]">
           PIXEL<span className="text-primary-500">CODE.</span>
