f8b21f5e156f7bdd612c6ada8edab3f3608beede
Eriko Syah <erikosyahputraf@gmail.com>
2024-10-04 15:47:26 +0700

    add : implement testi comp

--- src/pages/TestComponents.jsx
+++ src/pages/TestComponents.jsx
@@ -269,7 +269,7 @@ function App() {
         ))}
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-10">
-        <div>
+        <div className="space-y-20">
           {testimonials.map((testi, index) => (
             <Testi
               key={index}
