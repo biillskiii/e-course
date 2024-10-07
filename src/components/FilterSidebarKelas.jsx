7f616e58d4d6ea61376ac0502e372e55de43db1a
biillskiii <muhamad.nabil20052003@gmail.com>
2024-10-07 14:41:50 +0700

    build: filter sidebar kelas - comp

---
+++ src/components/FilterSidebarKelas.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import InputBase from "./InputForm";
+const FilterSidebar = ({ setFilteredCategories, setFilteredLevels }) => {
+  const [selectedCategories, setSelectedCategoriesState] = useState([]);
+  const [selectedLevels, setSelectedLevelsState] = useState([]);
+
+  const categories = [
+    "Semua Kategori",
+    "UI/UX Research & Design",
+    "Frontend Development",
+    "Backend Development",
+    "Data Science",
+  ];
+
+  const levels = ["Semua Level", "Pemula", "Menengah", "Ahli"];
+
+  const handleCategoryChange = (category) => {
+    const updatedCategories = selectedCategories.includes(category)
+      ? selectedCategories.filter((item) => item !== category)
+      : [...selectedCategories, category];
+
+    setSelectedCategoriesState(updatedCategories);
+    setFilteredCategories(updatedCategories);
+  };
+
+  const handleLevelChange = (level) => {
+    const updatedLevels = selectedLevels.includes(level)
+      ? selectedLevels.filter((item) => item !== level)
+      : [...selectedLevels, level];
+
+    setSelectedLevelsState(updatedLevels);
+    setFilteredLevels(updatedLevels);
+  };
+
+  const resetFilters = () => {
+    setSelectedCategoriesState([]);
+    setFilteredCategories([]); 
+    setSelectedLevelsState([]);
+    setFilteredLevels([]);
+  };
+
+  return (
+    <div className="p-10 border border-primary-200 rounded-3xl w-full">
+      {/* Kategori Filter */}
+      <FilterSection
+        title="Kategori"
+        options={categories}
+        selectedOptions={selectedCategories}
+        onOptionChange={handleCategoryChange}
+      />
+      {/* Level Filter */}
+      <FilterSection
+        title="Level"
+        options={levels}
+        selectedOptions={selectedLevels}
+        onOptionChange={handleLevelChange}
+      />
+      <button
+        onClick={resetFilters}
+        className="text-alert-danger text-base font-bold"
+      >
+        Reset Filter
+      </button>
+    </div>
+  );
+};
+
+const FilterSection = ({ title, options, selectedOptions, onOptionChange }) => {
+  return (
+    <div>
+      <h3 className="font-bold text-lg mb-2">{title}</h3>
+      <div>
+        {options.map((option, index) => (
+          <li key={index} className="flex items-center mb-4">
+            <InputBase
+              type="checkbox"
+              checked={selectedOptions.includes(option)}
+              onChange={() => onOptionChange(option)}
+            />
+            <label htmlFor={option} className="ml-2 text-gray-700">
+              {option}
+            </label>
+          </li>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FilterSidebar;
