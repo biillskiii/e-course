5d75fdba234b07a7f3beb936601930591fcdc3da
biillskiii <muhamad.nabil20052003@gmail.com>
2024-10-07 14:49:14 +0700

    fix : width searchbar and delete button "cari"

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,26 +1,26 @@
 import React from "react";
-import { SearchNormal1 } from "iconsax-react"; // Using react-icons for search icon
+import { SearchNormal1 } from "iconsax-react";
 
-const SearchBar = () => {
+const SearchBar = ({ setSearchTerm }) => {
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
-    <div className="flex items-center space-x-2">
-      {/* Search Input Container */}
-      <div className="flex items-center w-[737px] bg-white border border-gray-200 rounded-full px-3 shadow-sm">
-        {/* Search Icon */}
+    <div className="flex w-[824px] items-center space-x-2">
+      {/* Search Input */}
+      <div className="flex items-center w-full bg-white border border-gray-200 rounded-full px-3 shadow-sm">
+        {/* Icon */}
         <SearchNormal1 className="text-gray-500" />
 
-        {/* Input Field */}
+        {/* Input */}
         <input
           type="text"
           placeholder="Cari"
           className="border-none focus:outline-none focus:ring-0 text-gray-600 px-3 py-2"
+          onChange={handleInputChange}
         />
       </div>
-
-      {/* Search Button */}
-      <button className="w[31px] bg-primary-500 hover:bg-primary-600 text-white font-bold py-3 px-6 rounded-full focus:outline-none">
-        Cari
-      </button>
     </div>
   );
 };
