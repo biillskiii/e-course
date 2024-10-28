4f19f0661de56fc372928f847763931096f6d5d5
biillskiii <muhamad.nabil20052003@gmail.com>
2024-10-29 03:09:34 +0700

    add: page admin dashboard

---
+++ src/pages/admin/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../../components/Navbar";
+const Dashboard = () => {
+  return (
+    <div>
+      <Sidebar />
+      <Navbar variant="welcome" />
+    </div>
+  );
+};
+
+export default Dashboard;
