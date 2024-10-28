dc6302c4bb40626547bf1429bc409d35489b6b25
biillskiii <muhamad.nabil20052003@gmail.com>
2024-10-29 03:08:40 +0700

    add : data user dummy

---
+++ src/data.js
@@ -0,0 +1,7 @@
+export const userData = {
+  username: "John Doe",
+  avatar:
+    "https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-avatar-image-for-profile-png-image_13001882.png", // Using placeholder image as per requirements
+  notifications: 3,
+  isLoggedIn: true,
+};
