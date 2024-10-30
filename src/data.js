ccc95e8ae0c71af4229140c67f69c5182e299b7d
biillskiii <muhamad.nabil20052003@gmail.com>
2024-10-30 10:51:32 +0700

    add : data dummy

--- src/data.js
+++ src/data.js
@@ -5,3 +5,103 @@ export const userData = {
   notifications: 3,
   isLoggedIn: true,
 };
+export const statsCards = [
+  {
+    id: 1,
+    title: "Kelas",
+    value: "125",
+  },
+  {
+    id: 2,
+    title: "Webinar Aktif",
+    value: "28",
+  },
+  {
+    id: 3,
+    title: "Total Mentor",
+    value: "17823",
+  },
+  {
+    id: 4,
+    title: "Total Mentee",
+    value: "372",
+  },
+];
+
+// Data for popular classes
+export const popularClasses = [
+  {
+    id: 1,
+    title: "UI/UX Fundamental",
+    members: "11211",
+  },
+  {
+    id: 2,
+    title: "Basic Frontend Development",
+    members: "10211",
+  },
+  {
+    id: 3,
+    title: "Belajar Membuat Component",
+    members: "9985",
+  },
+];
+// Data for transactions
+export const transactions = [
+  {
+    id: "1276",
+    kodeProgram: "K3678",
+    namaProgram: "UI/UX Fundamental",
+    status: "Menunggu Pembayaran",
+    metode: "QRIS",
+  },
+  {
+    id: "1276",
+    kodeProgram: "W3678",
+    namaProgram: "Bangun Personal Brandingmu Sebagai UI/UX Designer",
+    status: "Menunggu Pembayaran",
+    metode: "VA",
+  },
+  {
+    id: "1276",
+    kodeProgram: "K3678",
+    namaProgram: "UI/UX Fundamental",
+    status: "Pembayaran Berhasil",
+    metode: "VA",
+  },
+  {
+    id: "1276",
+    kodeProgram: "K3678",
+    namaProgram: "UI/UX Fundamental",
+    status: "Pembayaran Berhasil",
+    metode: "e-Wallet",
+  },
+  {
+    id: "1276",
+    kodeProgram: "W3678",
+    namaProgram: "Bangun Personal Brandingmu Sebagai UI/UX Designer",
+    status: "Pembayaran Gagal",
+    metode: "VA",
+  },
+];
+export const kelasData = [
+  {
+    kode: "1276",
+    kategori: "UI/UX Research & Design",
+    nama: "UI/UX Fundamental",
+    mentee: "125",
+  },
+  {
+    kode: "1276",
+    kategori: "Frontend Development",
+    nama: "Basic Frontend Development",
+    mentee: "125",
+  },
+  {
+    kode: "1276",
+    kategori: "Frontend Development",
+    nama: "Dasar Pemrograman Javascript",
+    mentee: "125",
+  },
+  // ... tambahkan data lainnya sesuai kebutuhan
+];
