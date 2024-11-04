230de2e7d6fd1085e06278d23a8da935b04fbe27
biillskiii <muhamad.nabil20052003@gmail.com>
2024-11-04 16:22:19 +0700

    update : routing

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import LandingPage from "./pages/LandingPage";
 import SignUp from "./pages/SignUp";
 import TestComponents from "./pages/TestComponents";
 import Class from "./pages/Class";
-import Webinar from "./pages/Webinar";
 import ClassDetail from "./pages/ClassDetail";
 import Checkout from "./pages/Checkout";
 import ContactUs from "./pages/ContactUs";
 import Dashboard from "./pages/admin/Dashboard";
 import Kelas from "./pages/admin/Kelas";
+import Webinar from "./pages/admin/Webinar";
+import TambahKelas from "./pages/admin/TambahKelas";
+import StudentDashboard from "./pages/username/dashboard/Dashboard";
 // import DashboardUser from "./pages/username/dashboard/Dashboard";
 function App() {
   return (
@@ -29,6 +31,9 @@ function App() {
         <Route path="/hubungi-kami" element={<ContactUs />} />
         <Route path="/admin/dashboard" element={<Dashboard />} />
         <Route path="/admin/kelas" element={<Kelas />} />
+        <Route path="/admin/kelas/tambah-kelas" element={<TambahKelas />} />
+        <Route path="/admin/webinar" element={<Webinar />} />
+        <Route path="/dashboard-siswa" element={<StudentDashboard />} />
         {/* <Route path={`/${username}/dashboard`} element={<DashboardUser />} /> */}
       </Routes>
     </Router>
