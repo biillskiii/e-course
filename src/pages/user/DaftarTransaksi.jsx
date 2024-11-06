aeef6f0f6c31720d52013d1aa7111751e45b1a07
Eriko Syah <erikosyahputraf@gmail.com>
2024-11-06 13:14:29 +0700

    Add: DaftarTransaksi page

---
+++ src/pages/user/DaftarTransaksi.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "../../components/Button";
+import {
+  Category,
+  Home,
+  LogoutCurve,
+  Monitor,
+  Ticket,
+  Wallet,
+} from "iconsax-react";
+import NavbarDashboard from "../../components/NavbarDashboard";
+import {
+  userData,
+  userKelas,
+  sertifKelas,
+  userWebinar,
+  courseData,
+} from "../../data";
+import Card from "../../components/Card";
+import TransactionCard from "../../components/TransactionCard";
+
+const DaftarTransaksi = () => {
+  const navigate = useNavigate();
+
+  const handleNavigation = (path) => {
+    navigate(path);
+  };
+
+  return (
+    <section>
+      <div>
+        {/* Sidebar */}
+        <div className="w-60 fixed min-h-screen bg-white shadow-lg flex flex-col justify-between items-center p-5">
+          <div className="space-y-6">
+            <h1 className="mango text-center text-secondary-500 text-[40px] mb-10">
+              PIXEL<span className="text-primary-500">CODE.</span>
+            </h1>
+            <Button
+              label="Dashboard"
+              variant="side-primary"
+              leftIcon={<Home />}
+              size="very-big"
+              onClick={() => handleNavigation("/user/dashboard")}
+            />
+            <Button
+              label="Kelas"
+              variant="side-primary"
+              leftIcon={<Monitor />}
+              size="very-big"
+              onClick={() => handleNavigation("/user/kelas")}
+            />
+            <Button
+              label="Webinar"
+              variant="side-primary"
+              leftIcon={<Ticket />}
+              size="very-big"
+              onClick={() => handleNavigation("/user/webinar")}
+            />
+            <Button
+              label="Daftar Transaksi"
+              active={true}
+              variant="side-primary"
+              leftIcon={<Wallet />}
+              size="very-big"
+              onClick={() => handleNavigation("/user/daftar-transaksi")}
+            />
+            <Button
+              label="Pengaturan"
+              variant="side-primary"
+              leftIcon={<Category />}
+              size="very-big"
+              onClick={() => handleNavigation("/user/pengaturan")}
+            />
+          </div>
+          <div className="mt-20">
+            <Button
+              label="Keluar"
+              variant="side-danger"
+              leftIcon={<LogoutCurve />}
+              size="very-big"
+              onClick={() => handleNavigation("/masuk")}
+            />
+          </div>
+        </div>
+        <div className="w-full pl-60">
+          <NavbarDashboard
+            avatar={userData.avatar}
+            username={userData.username}
+          />
+
+          <div className="w-full flex flex-col p-10">
+            <div>
+              <h1>Daftar Transaksi</h1>
+              <TransactionCard course={courseData} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default DaftarTransaksi;
