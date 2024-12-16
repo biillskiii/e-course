6ec8e335da44d0e6151d386b358933f7a5439fef
Eriko Syah <erikosyahputraf@gmail.com>
2024-12-16 14:40:06 +0700

    Update: Integration daftar transaksi page

--- src/pages/user/DaftarTransaksi.jsx
+++ src/pages/user/DaftarTransaksi.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "../../components/Button";
 import {
@@ -19,12 +19,69 @@ import {
 } from "../../data";
 import Card from "../../components/Card";
 import TransactionCard from "../../components/TransactionCard";
+import { jwtDecode } from "jwt-decode";
 
 const DaftarTransaksi = () => {
   const navigate = useNavigate();
+  const [transaction, setTransaction] = useState([]);
+  const [visibleClasses, setVisibleClasses] = useState([]);
+  const [userData, setUserData] = useState(null); // Tambahkan state untuk data pengguna
+  const [isLoading, setIsLoading] = useState(true);
+  const [userProfile, setUserProfile] = useState("");
 
   const handleNavigation = (path) => {
     navigate(path);
+  };
+
+  const fetchClasses = async () => {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_LOCAL_API_KEY}/api/usertransactions`,
+        {
+          headers: {
+            Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
+          },
+        }
+      );
+      setIsLoading(false);
+      const result = await response.json();
+      setTransaction(result);
+    } catch (error) {
+      console.error("Error fetching classes:", error);
+    }
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetchClasses();
+  }, []);
+
+  useEffect(() => {
+    const token = sessionStorage.getItem("accessToken");
+    if (!token) {
+      navigate("/masuk");
+      return;
+    }
+    try {
+      const decodedToken = jwtDecode(token);
+      setUserProfile({
+        username: decodedToken.name || "",
+        avatar:
+          decodedToken.avatar ||
+          "https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-avatar-image-for-profile-png-image_13001882.png",
+      });
+    } catch (error) {
+      console.error("Error decoding token:", error);
+      navigate("/masuk");
+    }
+  }, [navigate]);
+
+  const handleCourses = () => {
+    navigate("/user/kelas");
+  };
+  const handleLogout = () => {
+    sessionStorage.removeItem("accessToken");
+    navigate("/masuk");
   };
 
   return (
@@ -79,23 +136,29 @@ const DaftarTransaksi = () => {
               variant="side-danger"
               leftIcon={<LogoutCurve />}
               size="very-big"
-              onClick={() => handleNavigation("/masuk")}
+              onClick={() => handleLogout("/masuk")}
             />
           </div>
         </div>
         <div className="w-full pl-60">
           <NavbarDashboard
-            avatar={userData.avatar}
-            username={userData.username}
+            avatar={userProfile?.avatar}
+            username={userProfile?.username}
           />
 
           <div className="w-full flex flex-col p-10">
             <div className="flex flex-col gap-8">
               <h1 className="font-bold text-3xl">Daftar Transaksi</h1>
-              {courseData.map((course, index) => (
-                <TransactionCard key={index} course={course} />
+              {transaction.map((user) => (
+                <TransactionCard
+                  img={user.course.path_photo}
+                  title={user.course.class_name}
+                  price={user.course.price}
+                  status={user.course.status}
+                  date={user.course.updated_at}
+                  transaction_id={user.transaction_id}
+                />
               ))}
-              ;
             </div>
           </div>
         </div>
